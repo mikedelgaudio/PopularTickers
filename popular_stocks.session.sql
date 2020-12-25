27f2fe07ee235ddff8dcf5291bce92f570a222e5
Mike DelGaudio <mdelgaud@stevens.edu>
2020-12-24 21:35:09 -0500

    adding in query for most freq; email settings

--- popular_stocks.session.sql
+++ popular_stocks.session.sql
@@ -1,9 +1,11 @@
-CREATE TABLE stocks(
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    ticker TEXT NOT NULL,
-    company TEXT NOT NULL,
-    price_change FLOAT NOT NULL,
-    buys INT NOT NULL,
-    sells INT NOT NULL,
-    time DATETIME DEFAULT CURRENT_TIMESTAMP
-)
+-- CREATE TABLE stocks(
+--     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+--     ticker TEXT NOT NULL,
+--     company TEXT NOT NULL,
+--     price_change FLOAT NOT NULL,
+--     buys INT NOT NULL,
+--     sells INT NOT NULL,
+--     time DATETIME DEFAULT CURRENT_TIMESTAMP
+-- )
+
+SELECT ticker, COUNT(*) AS frequency FROM stocks GROUP BY ticker ORDER BY frequency DESC 
--- scraper.py
+++ scraper.py
@@ -1,22 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 import sqlite3
+import time
+import schedule
+import smtplib
+from dotenv import load_dotenv
+import os
+  
 
-try:
-    conn = sqlite3.connect('popular_tickers.db')
-except Exception as err:
-    print("Error Connecting to Database")
-    conn.close()
-        
-try:
-    r = requests.get("https://eresearch.fidelity.com/eresearch/gotoBL/fidelityTopOrders.jhtml")
-    soup = BeautifulSoup(r.content)
-except Exception as err:
-    print("Error grabbing page: {0}".format(err))
-    conn.close()
 
 
 def main():
+    load_dotenv()
+    try:
+        conn = sqlite3.connect('popular_tickers.db')
+    except Exception as err:
+        print("Error Connecting to Database")
+        conn.close()
+
+    try:
+        r = requests.get("https://eresearch.fidelity.com/eresearch/gotoBL/fidelityTopOrders.jhtml")
+        soup = BeautifulSoup(r.content)
+    except Exception as err:
+        print("Error grabbing page: {0}".format(err))
+        conn.close()
+
     stocks = []
 
     for ticker_tag in soup.find_all('td', class_='second'):
@@ -41,12 +49,40 @@ def main():
         stocks[i].append(sell_amount)
 
     for stock in stocks:
-        addToDb(stock)
+        #addToDb(stock, conn)
+        pass
 
+    emailMe()
+    
+    conn.close()
 
-def addToDb(stock):
+def addToDb(stock, conn):
+    '''
+    If stock at [0] == ticker && price of previous day do not insert (means it was a holiday)
+    '''
     conn.execute("INSERT INTO stocks (ticker, company, price_change, buys, sells) VALUES (:ticker, :company, :price, :buys, :sells);", {'ticker': stock[0], 'company': stock[1], 'price': stock[2], 'buys': stock[3],'sells': stock[4]})
     conn.commit()
+    print("executed")
+
+def emailMe():
+    try: 
+        s = smtplib.SMTP(host="mail.delgaudiomike.com", port=465)
+        s.starttls()
+        x = s.login(os.environ.get("EMAIL_USER"), os.environ.get("EMAIL_PASS"))
+        print(x)
+    except Exception as err:
+        pass
+   
+
+def allMostPopular(conn):
+    c = conn.execute("SELECT ticker, COUNT(*) AS frequency FROM stocks GROUP BY ticker ORDER BY frequency DESC")
+    print(c.fetchall())
 
 main()
-conn.close()
+
+# schedule.every().day.at("17:00:00").do(main)
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(60)
+#     print("running")
